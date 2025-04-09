2f9c29d3e273141f4369eb25fa5e6eb5dd7f234d
StefanBcl <Stefan.a.buecherl@gmx.de>
2025-04-09 07:31:28 +0200

    h

--- examples/json_parser/HeyInput.py
+++ examples/json_parser/HeyInput.py
@@ -30,16 +30,12 @@ def analyse_paket(paket, struktur):
         for field, attributes in fields.items():
             bin_str=paket[attributes['offset']:attributes['offset']+attributes['length']]
             zahl = int(bin_str, 2)
-            if field!= "Source_IP" and field!="Destination_IP":
-                print(f"{field}: {zahl} in Binär {bin_str}")
-            elif field == "Source_IP" or field =="Destination_IP":
-                ip_parts = [str(int(bin_str[i:i+8], 2)) for i in range(0, 32, 8)]
-                ip_address = ".".join(ip_parts)
-                print(f"{field}: {ip_address} in Binär {bin_str}")
+            print(f"{field}: {zahl} in Binär {bin_str}")
+           
 
     return result
 
 # Hauptprogramm
 paket = lade_paket("examples/json_parser/paketHex.txt")
-ipv4_struktur = load_struktur("examples/json_parser/IPv4.json")
+ipv4_struktur = load_struktur("protocols/config/ipv4.json")
 analysis = analyse_paket(paket, ipv4_struktur)
